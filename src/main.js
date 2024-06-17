bef9dd77df99aaa839efd562bb9d8c6e6fe570aa
WenYaFei <1506825038@qq.com>
2024-06-17 20:02:47 +0800

    改了一下main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 // 新增引入
-import router from '././router'
+import router from './router'
 
 // 使用router
 createApp(App).use(router).mount('#app')
